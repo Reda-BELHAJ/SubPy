22a2ab8f5848bea66be7398c5ebd908d202b7770
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-21 00:17:26 +0100

    Light Fixes

--- Submarine.py
+++ Submarine.py
@@ -1,32 +1,44 @@
 import pygame
+import pygame.gfxdraw as gfx
 
 class Submarine:
     def __init__(self) :
         self.spriteR     = pygame.image.load('Assets/SubMarine.png')
         self.spriteL     = pygame.transform.flip(self.spriteR, True, False)
-        self.x           = 0
-        self.y           = 0
+        self.x           = 100
+        self.y           = 100
         self.sign        = 0
 
 
     def light(self, window):
-        xPlus, yplus = self.spriteR.get_size()
-        xP, yP       = 0, 0
+        xPlus, yPlus = self.spriteR.get_size()
+
         if self.sign == 0:
             xPlus += self.x
             xP     = xPlus + 160
+            xB     = xP - 40
         else:
             xPlus += self.x - xPlus
             xP     = xPlus - 160
+            xB     = xP + 40
         
-        yplus += self.y - 10
+        yPlus += self.y - 10
 
-        pygame.draw.polygon(
-            window,
-            (255, 255, 255, 0),
-            [
-            (xPlus,yplus),(xP,yplus + 80), (xP,yplus - 80)
-            ])
+        surf1     = pygame.Surface((800, 800), pygame.SRCALPHA)
+        surf2     = pygame.Surface((800, 800), pygame.SRCALPHA)
+        
+        pygame.draw.polygon(surf1,(255, 255, 255),[(xPlus,yPlus),(xP,yPlus + 80), (xP,yPlus - 80)])
+
+        # yB1 = 0.5 * xB + 47.5
+        # yB2 = - 0.5 * xB + 200.5
+
+        pygame.draw.polygon(surf2,(255, 255, 255),[(xPlus,yPlus),(xB,yPlus + 60), (xB,yPlus - 60)])
+
+        surf1.set_alpha(20)
+        window.blit(surf1, (0,0))
+
+        surf2.set_alpha(5)
+        window.blit(surf2, (0,0))
                 
 
     def move(self, window, velocity, acceleration):
